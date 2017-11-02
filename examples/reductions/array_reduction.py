a05f3d2bdac28433f6a06a7b8d581382dbf7d9d5
Karthik Senthil <skk3@illinois.edu>
2017-11-02 17:45:09 -0500

    Add assertions to array reductions example to verify correctness.
    Change-Id: I3beb42a45a0ed004fc16ca595a3a1f3e062a2608

--- examples/reductions/array_reduction.py
+++ examples/reductions/array_reduction.py
@@ -1,5 +1,13 @@
 from charmpy import charm, Mainchare, Array, Group, CkMyPe, CkNumPes, CkExit, ReadOnlies, CkAbort
 
+# utility methods for assertions
+def assert_allclose(actual, desired, tol):
+  assert len(actual) == len(desired)
+  assert sum([(abs(actual[i] - v) <= tol) for i,v in enumerate(desired)]) == len(actual)
+
+def assert_almost_equal(actual, desired, tol):
+  assert abs(actual -desired) <= tol
+
 ro = ReadOnlies()
 
 class Main(Mainchare):
@@ -19,44 +27,44 @@ class Main(Mainchare):
     for x in ro.ARRAY_SIZE: nElements *= x
     print "Running reduction example on", CkNumPes(), "processors for", nElements, "elements, array dims=", ro.ARRAY_SIZE
     ro.mainProxy = self.thisProxy
-    arrProxy = charm.TestProxy.ckNew(ro.ARRAY_SIZE)
+    ro.arrProxy = charm.TestProxy.ckNew(ro.ARRAY_SIZE)
     ro.groupProxy = charm.TestGroupProxy.ckNew()
-    arrProxy.doReduction()
+    ro.arrProxy.doReduction()
 
   def done_int(self, reduction_result):
-    print "[Main] All sum_int contributions done"
-    print "[Main] Total sum: ", reduction_result
+    assert reduction_result == 420, "Array-to-singleton sum_int reduction failed"
+    print "[Main] All sum_int contributions done. Test passed"
     self.recvdReductions += 1
     if (self.recvdReductions >= self.expectedReductions):
         CkExit()
 
   def done_nop(self):
-    print "[Main] All nop contributions received"
+    print "[Main] All nop contributions received. Test passed"
     self.recvdReductions += 1
     if (self.recvdReductions >= self.expectedReductions):
         CkExit()
 
   def done_float(self, reduction_result):
-    print "[Main] All sum_float contributions done"
-    print "[Main] Total sum: ", reduction_result
+    assert_allclose(reduction_result, [101.0, 134.0, 45.0], 1e-03)
+    print "[Main] All sum_float contributions done. Test passed"
     self.recvdReductions += 1
     if (self.recvdReductions >= self.expectedReductions):
         CkExit()
 
   def done_array_to_array(self):
-    print "[Main] All array-to-array contributions done"
+    print "[Main] All array-to-array contributions done. Test passed"
     self.recvdReductions += 1
     if (self.recvdReductions >= self.expectedReductions):
       CkExit()
 
   def done_array_to_array_bcast(self):
-    print "[Main] All array-to-array bcast contributions done"
+    print "[Main] All array-to-array bcast contributions done. Test passed"
     self.recvdReductions += 1
     if (self.recvdReductions >= self.expectedReductions):
       CkExit()
 
   def done_array_to_group(self):
-    print "[Main] All array-to-group contributions done"
+    print "[Main] All array-to-group contributions done. Test passed"
     self.recvdReductions += 1
     if (self.recvdReductions >= self.expectedReductions):
       CkExit()
@@ -64,7 +72,7 @@ class Main(Mainchare):
   def done_array_to_group_bcast(self):
     self.groupBcast += 1
     if self.groupBcast == CkNumPes():
-      print "[Main] All array-to-group bcast contributions done"
+      print "[Main] All array-to-group bcast contributions done. Test passed"
       self.recvdReductions += 1
       if (self.recvdReductions >= self.expectedReductions):
         CkExit()
@@ -93,12 +101,12 @@ class Test(Array):
     self.contribute([0, 8, 3], charm.ReducerType.sum_int, TestGroup.reduceFromArrayBcast, ro.groupProxy)
 
   def reductionTarget(self, reduction_result):
-    assert(self.thisIndex[0] == 0)
-    print "[Test ", self.thisIndex, "] Total sum: ", reduction_result
+    assert self.thisIndex[0] == 0
+    assert_almost_equal(reduction_result, 42.0, 1e-03)
     ro.mainProxy.done_array_to_array()
 
   def reductionTargetBcast(self, reduction_result):
-    print "[Test ", self.thisIndex, "] Total sum: ", reduction_result
+    assert_allclose(reduction_result, [42.0, 84.0], 1e-03)
     self.contribute(None, charm.ReducerType.nop, Main.done_array_to_array_bcast, ro.mainProxy)
 
 class TestGroup(Group):
@@ -107,11 +115,11 @@ class TestGroup(Group):
     print "TestGroup", self.thisIndex, "created on PE", CkMyPe()
 
   def reduceFromArray(self, reduction_result):
-    print "[TestGroup ", self.thisIndex, "] Total sum: ", reduction_result
+    assert reduction_result == 40, "Array-to-group sum_int reduction failed."
     ro.mainProxy.done_array_to_group()
 
   def reduceFromArrayBcast(self, reduction_result):
-    print "[TestGroup ", self.thisIndex, "] Total sum: ", reduction_result
+    assert reduction_result == [0, 80, 30], "Array-to-group bcast sum_int reduction failed."
     #TODO: add contribute support for groups
     #self.contribute(None, charm.ReducerType.nop, Main.done_array_to_group_bcast, ro.mainProxy)
     ro.mainProxy.done_array_to_group_bcast()
