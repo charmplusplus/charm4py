1b45a6121b512b883804ad38defb0791a769bf5c
Juan Galvez <jjgalvez@illinois.edu>
2019-01-25 08:27:27 -0600

    more tweaks

--- tests/topo/phynode_API.py
+++ tests/topo/phynode_API.py
@@ -2,13 +2,20 @@ from charm4py import charm
 
 
 def main(args):
-    pe = charm.firstPeOnPhysicalNode(0)
-    assert pe in list(range(charm.numPes()))
 
-    phyNodes = set([])
+    # phynode IDs should go from 0 to N-1, but assume that is not the case
+    phyNodes = set()
     for pe in range(charm.numPes()):
-        phyNodes.add(charm.physicalNodeID(pe))
+        phynode = charm.physicalNodeID(pe)
+        assert phynode >= 0
+        phyNodes.add(phynode)
+
     assert len(phyNodes) == charm.numPhysicalNodes()
+
+    for phynode in phyNodes:
+        pe = charm.firstPeOnPhysicalNode(phynode)
+        assert pe >= 0 and pe < charm.numPes()
+        assert charm.physicalNodeID(pe) == phynode
 
     exit()
 
