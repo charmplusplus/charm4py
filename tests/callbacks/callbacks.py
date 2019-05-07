8c1df3d23199f4942277d83fc8a4d67f0faa3663
Juan Galvez <jjgalvez@illinois.edu>
2019-05-07 14:59:48 -0500

    Added a test for callbacks
    The program tests sending callback objects to remote chares and
    getting results back via the callbacks. It tests both futures
    and proxy methods as callbacks.

---
+++ tests/callbacks/callbacks.py
@@ -0,0 +1,69 @@
+from charm4py import charm, Chare, Group, Array, Reducer, threaded
+import sys
+
+
+class Controller(Chare):
+
+    @threaded
+    def start(self, workers, callback):
+        f = charm.createFuture()
+        workers.work(f)
+        result = f.get()
+        callback(result)
+
+
+class Worker(Chare):
+
+    def work(self, callback):
+        self.contribute(self.thisIndex, Reducer.sum, callback)
+
+
+class CallbackReceiver(Chare):
+
+    def __init__(self, main):
+        self.main = main
+
+    def getResult(self, result):
+        #print('[' + str(charm.myPe()) + '] got result:', result)
+        assert result == (charm.numPes() * (charm.numPes() - 1)) // 2
+        self.main.workDone(self.thisIndex[0])
+
+    def getResultBroadcast(self, result):
+        #print('[' + str(charm.myPe()) + '] got result:', result)
+        assert result == (charm.numPes() * (charm.numPes() - 1)) // 2
+        self.contribute(1, Reducer.sum, self.main.workDone)
+
+
+class Main(Chare):
+
+    def __init__(self, args):
+        if sys.version_info < (3, 0, 0):  # not supported in Python 2.7
+            exit()
+        assert charm.numPes() >= 4
+        self.done = -1
+        workers = Group(Worker)
+        controllers = Array(Controller, charm.numPes())
+        receivers = Array(CallbackReceiver, charm.numPes(), args=[self.thisProxy])
+        workers.work(receivers[1].getResult)
+        self.wait('self.done == 1')
+        self.done = -1
+
+        controllers[1].start(workers, receivers[2].getResult)
+        self.wait('self.done == 2')
+        self.done = -1
+
+        controllers[2].start(workers, receivers.getResultBroadcast)
+        self.wait('self.done == ' + str(charm.numPes()))
+        self.done = -1
+
+        f = charm.createFuture()
+        controllers[3].start(workers, f)
+        assert f.get() == (charm.numPes() * (charm.numPes() - 1)) // 2
+
+        exit()
+
+    def workDone(self, src_info):
+        self.done = src_info
+
+
+charm.start(Main)
