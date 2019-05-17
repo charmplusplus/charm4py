4137393fd4069cbeb5e8a6e8130fac0b0220bb74
Juan Galvez <jjgalvez@illinois.edu>
2019-05-17 09:58:50 -0500

    Fixed chare_migration.py test by calling migrate() as entry method

--- tests/migration/chare_migration.py
+++ tests/migration/chare_migration.py
@@ -14,13 +14,13 @@ class Migrate(Chare):
     A class to test the migration of chares.
     """
 
-    def test(self):
+    def migrated(self):
         """
         Test method called after migration to assert that the
         chare has migrated.
         """
         if self.thisIndex == (0,):
-            print("Test called on PE ", charm.myPe())
+            print(self.thisIndex, 'migrated to PE', charm.myPe())
         assert charm.myPe() == self.toPe
         self.contribute(None, None, charm.thisProxy[0].exit)
 
@@ -29,15 +29,14 @@ class Migrate(Chare):
         Invoke the starter code for test.
         """
         if charm.myPe() == 0:
-            print("On PE", charm.myPe(), "before migration")
-        self.thisProxy[self.thisIndex].test()
+            print(self.thisIndex, 'on PE', charm.myPe(), 'before migration')
         self.toPe = (charm.myPe() + 1) % charm.numPes()
-        self.migrate(self.toPe)
+        self.thisProxy[self.thisIndex].migrate(self.toPe)
 
 
 def main(args):
     if charm.numPes() == 1:
-        charm.abort("Run program with more than 1 PE")
+        charm.abort('Run program with more than 1 PE')
     array_proxy = Array(Migrate, CHARES_PER_PE * charm.numPes())
     array_proxy.start()
 
