e878c95e87aec197dd602a417e253bb094579232
juanjgalvez <jjgalvez@illinois.edu>
2019-08-25 14:17:30 -0500

    charmlib: Restore use of CmiPrintf and CmiAbort for all charmlib layers

--- charm4py/charmlib/charmlib_cffi_build.py
+++ charm4py/charmlib/charmlib_cffi_build.py
@@ -105,8 +105,8 @@ ffibuilder.cdef("""
     int CkMyPeHook();
     int CkNumPesHook();
     void realCkExit(int exitcode);
-    void CmiAbortHook(const char *);
-    void CmiPuts(const char *);
+    void CmiAbort(const char *, ...);
+    void CmiPrintf(const char *, ...);
     void LBTurnInstrumentOn();
     void LBTurnInstrumentOff();
     void free(void *ptr);
