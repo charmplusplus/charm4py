681129cd918f8eed6f71d87e3ba0af8d9024738c
Zane Fink <finkzane@gmail.com>
2020-12-30 13:02:54 -0500

    Added correct print format, pinned memory now used for staging host memory

--- tests/benchmark/pingpong_gpu.py
+++ tests/benchmark/pingpong_gpu.py
@@ -3,66 +3,78 @@ import time
 import numpy as np
 from numba import cuda
 
-class Ping(Chare):
+USE_PINNED = True
 
-    #TODO: How do we determine how many chares?
+class Ping(Chare):
     def __init__(self, use_gpudirect, print_format):
         self.gpu_direct = use_gpudirect
         self.num_chares = charm.numPes()
         self.print_format = print_format
-        # self.am_low_chare = self.thisIndex < self.num_chares // 2
         self.am_low_chare = self.thisIndex == 0
 
         if self.am_low_chare:
-            print("Msg Size, Iterations, One-way Time (us), Bandwidth (bytes/us)")
+            if print_format == 0:
+                print("Msg Size, Iterations, One-way Time (us), "
+                      "Bandwidth (bytes/us)"
+                      )
+            else:
+                print(f'{"Msg Size": <30} {"Iterations": <25} '
+                      f'{"One-way Time (us)": <20} {"Bandwidth (bytes/us)": <20}'
+                      )
 
     @coro
     def do_iteration(self, message_size, num_iters, done_future):
-        # TODO: How do we allocate device data again?
-        # dev_array = cuda.zeros(on_device)
-        # host_data = cuda.zeros(on_host)
-        data = np.zeros(message_size, dtype='int8')
+        if USE_PINNED:
+            h_data = cuda.pinned_array(message_size, dtype='int8')
+        else:
+            h_data = np.zeros(message_size, dtype='int8')
+        d_data = cuda.device_array(message_size, dtype='int8')
+        d_data.copy_to_device(h_data)
         partner_idx = int(not self.thisIndex)
-        # partner = self.thisProxy[self.thisIndex + self.num_chares // 2]
         partner = self.thisProxy[partner_idx]
         partner_channel = Channel(self, partner)
 
         tstart = time.time()
 
-
         for _ in range(num_iters):
             if self.am_low_chare:
                 if not self.gpu_direct:
-                    dev_array = 0 # copy the device array to memory on device, TODO: use pinned memory?
-                # partner_channel.send(dev_array)
-                partner_channel.send(data)
-                partner_channel.recv()
+                    d_data.copy_to_host(h_data)
+                    # partner_channel.send(dev_array)
+                    partner_channel.send(h_data)
+                    d_data.copy_to_device(partner_channel.recv())
+                else:
+                    raise NotImplementedError("TODO: GPU Direct")
 
-            # if not self.gpu_direct:
             else:
-                partner_channel.recv()
-                partner_channel.send(data)
-                # copy the data back to the device
+                if not self.gpu_direct:
+                    d_data.copy_to_device(partner_channel.recv())
+                    d_data.copy_to_host(h_data)
+                    partner_channel.send(h_data)
+                else:
+                    raise NotImplementedError("TODO: GPU Direct")
 
-        # TODO: should we have barrier (reduction) here?
         tend = time.time()
 
         elapsed_time = tend - tstart
 
         if self.am_low_chare:
-            # display data here
             self.display_iteration_data(elapsed_time, num_iters, message_size)
 
         self.reduce(done_future)
 
     def display_iteration_data(self, elapsed_time, num_iters, message_size):
-        elapsed_time /= 2 # 1-way performance, not RTT
-        elapsed_time /= num_iters # Time for each message
+        elapsed_time /= 2  # 1-way performance, not RTT
+        elapsed_time /= num_iters  # Time for each message
         bandwidth = message_size / elapsed_time
         if self.print_format == 0:
-            print(f'{message_size},{num_iters},{elapsed_time * 1e6},{bandwidth / 1e6}')
+            print(f'{message_size},{num_iters},{elapsed_time * 1e6},'
+                  f'{bandwidth / 1e6}'
+                  )
         else:
-            print('Not implemented!')
+            print(f'{message_size: <30} {num_iters: <25} '
+                  f'{elapsed_time * 1e6: <20} {bandwidth / 1e6: <20}'
+                  )
 
 def main(args):
     if len(args) < 7:
