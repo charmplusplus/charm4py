46851e68a96dfa69e95cfc354bc4f39a6979790b
Zane Fink <finkzane@gmail.com>
2021-04-22 15:47:35 -0400

    tests for multiple arrays

---
+++ tests/cuda/multi_array.py
@@ -0,0 +1,85 @@
+from charm4py import charm, Chare, Array, coro, Future, Channel, Group, ArrayMap
+import numpy as np
+from numba import cuda
+import array
+
+
+class A(Chare):
+    def __init__(self, msg_size):
+        self.msg_size = msg_size
+
+    @coro
+    def run(self, done_future, addr_optimization=False):
+        partner = self.thisProxy[int(not self.thisIndex[0])]
+        partner_channel = Channel(self, partner)
+
+        device_data = cuda.device_array(self.msg_size, dtype='int8')
+        device_data2 = cuda.device_array(self.msg_size, dtype='int8')
+        # if addr_optimization:
+        d_addr = array.array('L', [0, 0])
+        d_size = array.array('L', [0, 0])
+
+        d_addr[0] = device_data.__cuda_array_interface__['data'][0]
+        d_addr[1] = device_data2.__cuda_array_interface__['data'][0]
+
+        d_size[0] = device_data.nbytes
+        d_size[1] = device_data2.nbytes
+
+        host_array = np.array(self.msg_size, dtype='int32')
+        host_array.fill(42)
+
+        if self.thisIndex[0]:
+            h1 = np.ones(self.msg_size, dtype='int8')
+            h2 = np.zeros(self.msg_size, dtype='int8')
+            device_data.copy_to_device(h1)
+            device_data2.copy_to_device(h2)
+            if addr_optimization:
+                partner_channel.send(20, host_array, gpu_src_ptrs=d_addr,
+                                     gpu_src_sizes=d_size
+                                     )
+            else:
+                partner_channel.send(20, host_array, device_data, device_data2)
+        else:
+            if addr_optimization:
+                f, g = partner_channel.recv(device_data, device_data2)
+            else:
+                f, g = partner_channel.recv(post_buf_addresses=d_addr,
+                                            post_buf_sizes=d_addr
+                                            )
+            h1 = device_data.copy_to_host()
+            h2 = device_data2.copy_to_host()
+
+            assert f == 20
+            assert np.array_equal(host_array, g)
+            assert np.array_equal(h1, np.ones(self.msg_size, dtype='int8'))
+            assert np.array_equal(h2, np.zeros(self.msg_size, dtype='int8'))
+        self.reduce(done_future)
+
+
+class ArrMap(ArrayMap):
+    def procNum(self, index):
+        return index[0] % 2
+
+
+def main(args):
+    # if this is not a cuda-aware build,
+    # vacuously pass the test
+    if not charm.CkCudaEnabled():
+        print("WARNING: Charm4Py was not build with CUDA-enabled Charm++. "
+              "GPU-Direct functionality will not be tested"
+              )
+        charm.exit(0)
+
+    peMap = Group(ArrMap)
+    chares = Array(A, 2, args=[8192], map=peMap)
+    done_fut = Future()
+    chares.run(done_fut, addr_optimization=False)
+    done_fut.get()
+
+    done_fut = Future()
+    chares.run(done_fut, addr_optimization=True)
+    done_fut.get()
+    charm.exit(0)
+
+
+charm.start(main)
