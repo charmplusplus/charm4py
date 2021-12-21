91cce95b7fb292703700336c9d891434b8ce8c08
Zane Fink <finkzane@gmail.com>
2021-12-21 14:24:32 -0800

    clean up

--- examples/cannon/cannon.py
+++ examples/cannon/cannon.py
@@ -4,17 +4,15 @@ import numpy as np
 from math import sqrt
 
 try:
-    from numba import jit
-    numbaFound = True
+    from numba import njit
 except ImportError:
-    numbaFound = False
-    # create a dummy numba.jit decorator
-    def jit(*args, **kwargs):
-        def deco(func):
-            return func
-        return deco
-    njit = jit
+    # create a dummy numba.njit decorator
+    def njit(func):
+        return func
 
+@njit
+def matmul(C, A, B):
+    C += A @ B
 
 class SubMatrix(Chare):
     def __init__(self, subdim_size, charedim, init_done):
@@ -28,19 +26,15 @@ class SubMatrix(Chare):
         self.sub_b = np.ones((subdim_size, subdim_size), dtype=np.float64)
         self.sub_b[:,:] = (charedim*self.thisIndex[0]) + self.thisIndex[1]
 
-        self.recv_a = np.ndarray((subdim_size, subdim_size), dtype=np.float64)
-        self.recv_b = np.ndarray((subdim_size, subdim_size), dtype=np.float64)
+        self.recv_a = np.ndarray((subdim_size,subdim_size), dtype=np.float64)
+        self.recv_b = np.ndarray((subdim_size,subdim_size), dtype=np.float64)
 
         self.sub_c = np.zeros((subdim_size, subdim_size), dtype=np.float64)
 
-        if self.thisIndex == (0,0):
-            self.whole_a = None
-            self.whole_b = None
-            self.whole_c = None
+        warmup_c = np.zeros((subdim_size, subdim_size), dtype=np.float64)
 
-        if verify:
-            self.reduce(self.thisProxy[(0,0)].display, self.sub_a, Reducer.gather)
-            self.reduce(self.thisProxy[(0,0)].display, self.sub_b, Reducer.gather)
+        # ensure the kernel is compiled
+        matmul(warmup_c, self.sub_a, self.sub_b)
 
         self.reduce(init_done)
 
@@ -50,10 +44,10 @@ class SubMatrix(Chare):
                                                       self.thisProxy[target_idx]
                                                       )
         return self.neighbor_cache[target_idx]
+
     @coro
     def cannons_multiplication(self, mult_done_future):
         # do initial shift
-
         # left-shift
         if self.thisIndex[0] > 0:
             self.shift(0, self.thisIndex[0])
@@ -65,7 +59,7 @@ class SubMatrix(Chare):
             self.sub_b, self.recv_b = self.recv_b, self.sub_b
 
         # todo multiplication kernel, will be interesting to see how they compare
-        self.sub_c = self.sub_a @ self.sub_b
+        matmul(self.sub_c, self.sub_a, self.sub_b)
 
         for iter in range(self.charedim - 1):
             self.shift(0, 1)
@@ -74,35 +68,11 @@ class SubMatrix(Chare):
             self.sub_a, self.recv_a = self.recv_a, self.sub_a
             self.sub_b, self.recv_b = self.recv_b, self.sub_b
 
-            self.sub_c += self.sub_a @ self.sub_b
+            matmul(self.sub_c, self.sub_a, self.sub_b)
 
-
-        if verify:
-            self.reduce(self.thisProxy[(0,0)].display, self.sub_c, Reducer.gather)
         self.reduce(mult_done_future)
 
-    def display(self, matr):
-        ls = list()
-        for i in range(int(len(matr)**0.5)):
-            l = list()
-            for j in range(int(len(matr)**0.5)):
-                l.append(matr[i*(int(len(matr)**0.5))+j])
-            ls.append(l)
-        nd = np.block(ls)
-        if self.whole_a is None:
-            self.whole_a = nd
-        elif self.whole_b is None:
-            self.whole_b = nd
-        elif self.whole_c is None:
-            self.whole_c = nd
-        if not(self.whole_a is None or self.whole_b is None or self.whole_c is None):
-            self.whole_c = self.whole_a @ self.whole_b
-            try:
-                assert np.allclose(nd, self.whole_c)
-            except AssertionError:
-                print("ERROR: computed output matrix does not match expected")
-
-    # the communication routines should be optimized so both sends/receives can complte in parallel
+    # the communication routines should be optimized so both sends/receives can complete in parallel
     def shift(self, up_shift, left_shift):
         send_target_idx = ((self.thisIndex[0] - up_shift) % self.charedim,
                            (self.thisIndex[1] - left_shift) % self.charedim
@@ -116,33 +86,35 @@ class SubMatrix(Chare):
 
         if left_shift:
             send_ch.send(self.sub_a)
-            self.recv_a = recv_ch.recv()
+            if self.zerocopy:
+                recv_ch.recv(self.recv_a)
+            else:
+                self.recv_a = recv_ch.recv()
         if up_shift:
             send_ch.send(self.sub_b)
-            self.recv_b = recv_ch.recv()
+            if self.zerocopy:
+                recv_ch.recv(self.recv_b)
+            else:
+                self.recv_b = recv_ch.recv()
+
 
 
 def main(args):
     if len(args) < 3:
-        print(f"USAGE: {args[0]} matrix_dim chare_dim verify")
+        print(f"USAGE: {args[0]} matrix_dim chare_dim")
+        charm.exit(1)
     matrix_dim = int(args[1])
     chare_dim = int(args[2])
-    verify = False
 
-    if len(args) == 4:
-        verify = int(args[3])
-
-    assert (chare_dim) % 1 == 0
-    matr_x, matr_y = matrix_dim, matrix_dim
     if matrix_dim ** 2 % chare_dim:
         print("ERROR: Matrix dim must evenly divide chare dim.")
         charm.exit(1)
 
-    charm.thisProxy.updateGlobals({'verify':verify}, awaitable=True).get()
     # size of each chare's sub-matrix
     subdim_size = matrix_dim / (chare_dim)
     assert subdim_size % 1 == 0
     subdim_size = int(subdim_size)
+    print(f"Size of each chare's sub-array: {8*(subdim_size**2)/(1024**2)}MiB")
 
     init_done = Future()
     chares = Array(SubMatrix, (chare_dim, chare_dim),
@@ -158,9 +130,5 @@ def main(args):
 
     print(f"Elapsed time: {tend-tstart}")
     charm.exit()
-
-
-
-
 
 charm.start(main)
