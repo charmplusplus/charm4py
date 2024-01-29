e1ed79b9a5503e4ce8f9eff264ec1e9be3deccef
Ritvik Rao <rao1@dt-login01.delta.ncsa.illinois.edu>
2024-01-29 08:47:05 -0600

    pool example

---
+++ examples/pool/pool_simple.py
@@ -0,0 +1,13 @@
+from charm4py import charm
+
+def square(x):
+    return x**2
+
+def main(args):
+    result = charm.pool.map_async(square, range(10), chunksize=2, multi_future=True)
+    for element in result:
+        print(element.get())
+    #print(result)  # prints [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+    exit()
+
+charm.start(main)
