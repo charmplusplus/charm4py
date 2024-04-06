5ea2bcfaaee3065f6aca5d5908bab5318fb20bc0
Aditya Bhosale <adityapb1546@gmail.com>
2024-04-06 16:39:37 -0500

    Fix issues when objects are numpy arrays

--- examples/ray/simple.py
+++ examples/ray/simple.py
@@ -1,5 +1,6 @@
 from charm4py import charm, coro, Chare, Array, ray
 from time import sleep
+import numpy as np
 
 
 @ray.remote
@@ -26,14 +27,19 @@ def main(args):
     ray.init()
     # create 3 instances of MyChare, distributed among cores by the runtime
     arr = [Compute.remote(i) for i in range(4)]
-
+    
+    obj1 = np.arange(100)
+    obj2 = np.arange(100)
+    a = ray.put(obj1)
+    b = ray.put(obj2)
     c = arr[0].add.remote(1, 2) # fut id 0
     d = arr[1].add.remote(3, c) # fut id 1
     e = arr[2].add.remote(2, d)
     f = arr[3].add.remote(c, 4)
-    g = add_task.remote(e, f)
+    g = arr[3].add.remote(a, b)
+    h = add_task.remote(e, f)
 
-    not_ready = [c, d, e, f, g]
+    not_ready = [c, d, e, f, g, h]
     while len(not_ready) > 0:
         ready, not_ready = ray.wait(not_ready)
         print("Fetched value: ", ray.get(ready))
