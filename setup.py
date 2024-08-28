652bc79aa70a9458f285f8756b593fc973967e38
Maya Taylor <70495835+mayantaylor@users.noreply.github.com>
2024-08-28 15:52:05 -0500

    Modernize build system to use pyproject.toml and build wheels (#267)
    * greenlet 3.0.0 drops support for python 3.6
    
    * draft pyproject.toml
    
    * adding templated build wheel yaml
    
    * set up build wheel action
    
    manually specifying package dir
    
    formatting
    
    typo
    
    manual cibuildwheel command?
    
    one dir down?
    
    adding debug info
    
    typo
    
    testing git installation
    
    clean up print statements
    
    typos
    
    removing 'build' dependency
    
    separating workflows
    
    adding testpypi workflow
    
    * set up testpypi upload action
    
    * don't need cmake? not used
    
    * cleanup
    
    * adding pypy builds
    
    * work around pypy dev0 checkout
    
    * adding comments
    
    * updating installation instructions
    
    * action to publish to pypi
    
    * tmp commit to get action to flush
    
    * tmp commit to get action to flush on PR
    
    * restoring upload action
    
    * adding source dist
    
    * fetch full depth
    
    * adding source dist to upload
    
    * line break
    
    * experiment without github charm token
    
    * cleanup - somehow checkout charm_src got duplicated
    
    * remove manual requirements installation instructions (handled by pip install)
    
    ---------
    
    Co-authored-by: Ritvik Rao <rsrao2@illinois.edu>

--- setup.py
+++ setup.py
@@ -118,7 +118,13 @@ def check_libcharm_version(charm_src_dir):
 def check_cffi():
     try:
         import cffi
-        version = tuple(int(v) for v in cffi.__version__.split('.'))
+        version_str = cffi.__version__.split('.')
+        
+        # pypy3.9 returns version string like '1.17.0.dev0'
+        if (len(version_str) > 3): 
+            version_str = version_str[:3]
+            
+        version = tuple(int(v) for v in version_str)
         if version < (1, 7):
             raise DistutilsSetupError('Charm4py requires cffi >= 1.7. '
                                       'Installed version is ' + cffi.__version__)
@@ -369,42 +375,12 @@ if os.environ.get('CHARM4PY_BUILD_CFFI') == '1':
     additional_setup_keywords['cffi_modules'] = 'charm4py/charmlib/charmlib_cffi_build.py:ffibuilder'
 
 
-with open('README.rst', 'r') as f:
-    long_description = f.read()
-
-
 setuptools.setup(
-    name='charm4py',
     version=charm4py_version,
-    author='Juan Galvez and individual contributors',
-    author_email='jjgalvez@illinois.edu',
-    description='Charm4py Parallel Programming Framework',
-    long_description=long_description,
-    url='https://github.com/charmplusplus/charm4py',
-    keywords='parallel parallel-programming distributed distributed-computing hpc HPC runtime',
     packages=setuptools.find_packages(),
     package_data={
         'charm4py': ['libcharm_version'],
     },
-    entry_points={
-        'console_scripts': [
-            'charmrun = charmrun.start:start',
-        ],
-    },
-    install_requires=['numpy>=1.10.0', 'greenlet>=3.0.0', 'cython>=3.0.0', 'cmake'],
-    python_requires='~=3.6',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'License :: Free for non-commercial use',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: POSIX',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: System :: Distributed Computing',
-        'Topic :: System :: Clustering',
-    ],
     ext_modules=extensions,
     cmdclass = {'build_py': custom_build_py,
                 'build_ext': custom_build_ext,
