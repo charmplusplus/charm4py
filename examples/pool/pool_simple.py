a2cdf2d0ae1a4a6d81fc9285c9a7b00d0438b76f
Ritvik Rao <rsrao2@illinois.edu>
2024-10-14 18:26:06 -0500

    fix pool simple example

--- examples/pool/pool_simple.py
+++ examples/pool/pool_simple.py
@@ -13,7 +13,10 @@ def main(args):
         print("Error: Run with more than one PE (exiting). For documentation on using pools, see: https://charm4py.readthedocs.io/en/latest/pool.html")
         exit()
     results = charm.pool.map_async(square, [4], chunksize=1, multi_future=True)
-    results_twice = charm.pool.map_async(twice, results, chunksize=1, multi_future=True)
+    real_results = []
+    for item in results:
+        real_results.append(item.get())
+    results_twice = charm.pool.map_async(twice, real_results, chunksize=1, multi_future=True, is_ray=True)
 
     for x in results_twice:
         print(x.get())
