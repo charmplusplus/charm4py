a2dbc06f6d65a9428ec1f0d2eefdcdae41fec8f3
Ritvik Rao <rsrao2@illinois.edu>
2024-10-15 10:41:32 -0500

    fix futures bug

--- examples/pool/pool_simple.py
+++ examples/pool/pool_simple.py
@@ -12,11 +12,8 @@ def main(args):
     if(charm.numPes()==1):
         print("Error: Run with more than one PE (exiting). For documentation on using pools, see: https://charm4py.readthedocs.io/en/latest/pool.html")
         exit()
-    results = charm.pool.map_async(square, [4], chunksize=1, multi_future=True)
-    real_results = []
-    for item in results:
-        real_results.append(item.get())
-    results_twice = charm.pool.map_async(twice, real_results, chunksize=1, multi_future=True, is_ray=True)
+    results = charm.pool.map_async(square, [4], chunksize=1, multi_future=True, is_ray=True)
+    results_twice = charm.pool.map_async(twice, results, chunksize=1, multi_future=True, is_ray=True)
 
     for x in results_twice:
         print(x.get())
