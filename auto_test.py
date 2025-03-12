514347ddd1d1de8353abdf968e6fb222858d171d
Ritvik Rao <rsrao2@illinois.edu>
2025-03-12 11:38:29 -0500

    fix macos14 run

--- auto_test.py
+++ auto_test.py
@@ -61,6 +61,8 @@ for test in tests:
             continue
     if 'timeout_override' in test:
         TIMEOUT = test['timeout_override']
+    else:
+        TIMEOUT = 120
     num_processes = max(test.get('force_min_processes', default_num_processes), default_num_processes)
     for interface in interfaces:
         durations[interface][test['path']] = []
