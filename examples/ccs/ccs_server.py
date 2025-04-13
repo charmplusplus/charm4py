bba3e1c23c66fa40cc9331b16fb919fb4c0e28d7
Ritvik Rao <rsrao2@illinois.edu>
2025-04-12 20:36:23 -0500

    fix registration of handler

--- examples/ccs/ccs_server.py
+++ examples/ccs/ccs_server.py
@@ -1,4 +1,11 @@
-from charm4py import charm, Chare, Array
+from charm4py import charm, Chare, Array, Future, Reducer, Group
+
+class RegisterPerChare(Chare):
+
+    def register(self, return_future):
+        charm.CcsRegisterHandler("ping2", handler)
+        charm.CcsRegisterHandler("ping", handler)
+        self.reduce(return_future, 0, Reducer.sum)
 
 def handler(msg):
     if charm.CcsIsRemoteRequest():
@@ -10,8 +17,10 @@ def handler(msg):
 def main(args):
     # No need to initialize converse, because charm.start does this
     # just register the handler
-    charm.CcsRegisterHandler("ping2", handler)
-    charm.CcsRegisterHandler("ping", handler)
+    reg_wait = Future()
+    registers = Group(RegisterPerChare)
+    registers.register(reg_wait)
+    reg_wait.get()
     print("CCS Handlers registered . Waiting for net requests...")
 
 
