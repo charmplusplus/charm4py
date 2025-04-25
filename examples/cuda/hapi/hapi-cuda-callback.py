e489a75dcf62006ffcb6041b8271f8872cf84652
Zane Fink <finkzane@gmail.com>
2025-04-25 12:32:21 -0500

    Make example numba

--- examples/cuda/hapi/hapi-cuda-callback.py
+++ examples/cuda/hapi/hapi-cuda-callback.py
@@ -1,36 +1,47 @@
 from charm4py import charm
 import time
-import torch
+import numba.cuda as cuda
+import numpy as np
 
-# using numba requires the following stream handle
-# import numba.cuda as cuda
-# s = cuda.stream()
-# stream_handle = s.handle.value
+@cuda.jit
+def elementwise_sum_kernel(x_in, x_out):
+    idx = cuda.grid(1)
+    if idx < x_in.shape[0]:
+        x_out[idx] = x_in[idx] + x_in[idx]
 
-def main(args):        
-    cuda = torch.device('cuda')
-    s = torch.cuda.Stream()  # Create a new stream.
-    A = torch.empty((100, 100), device=cuda).normal_(0.0, 1.0)
-   
-    print("Starting computation and inserting callback")
+def main(args):
+    N = 1_000_000
+    array_size = (N,)
+
+    s = cuda.stream()
+    stream_handle = s.handle.value
+
+    A_host = np.arange(N, dtype=np.float32)
+
+    A_gpu = cuda.device_array(array_size, dtype=np.float32, stream=s)
+    B_gpu = cuda.device_array(array_size, dtype=np.float32, stream=s)
+    A_gpu.copy_to_device(A_host, stream=s)
+
+    threads_per_block = 128
+    blocks_per_grid = (N + (threads_per_block - 1)) // threads_per_block
+
+    print("Launching kernel and inserting callback...")
     start_time = time.perf_counter()
-    with torch.cuda.stream(s):
-        B = torch.sum(A)
-    
-    # create future to track cuda stream
+    elementwise_sum_kernel[blocks_per_grid, threads_per_block, s](A_gpu, B_gpu)
+
     return_fut = charm.Future()
-    stream_handle = s.cuda_stream
-    charm.lib.hapiAddCudaCallback(stream_handle, return_fut) 
-    
-    # other work can be overlapped with kernel here
-    
+    charm.hapiAddCudaCallback(stream_handle, return_fut)
     return_fut.get()
-    
-    sum = B.cpu().item()
-    elapsed = time.perf_counter() - start_time
-    print(f"Kernel done in {elapsed} seconds. Sum result is {sum}")
-    
+    kernel_done_time = time.perf_counter()
+    print(f"Callback received, kernel finished in {kernel_done_time - start_time:.6f} seconds.")
+
+    B_host = B_gpu.copy_to_host(stream=s)
+
+    s.synchronize()
+
+    sum_result = np.sum(B_host)
+    print(f"Sum of result is {sum_result}")
+
     charm.exit()
-    
 
 charm.start(main)
