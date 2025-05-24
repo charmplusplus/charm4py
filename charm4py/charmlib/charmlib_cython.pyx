e4c23a568e4ef602215317908abf4d8b27c39da2
Ritvik Rao <rsrao2@illinois.edu>
2025-05-23 21:17:03 -0400

    Projections support (#290)
    * add pairwise addition/product
    
    * add logical reductions
    
    * add tests for section-based reductions
    
    * wip
    
    * reductions are now optimized for numpy types
    
    * numpy reductions always preferred when available
    
    * added checks that single-value reductions work with sections
    
    * add section reduction test
    
    * no fstrings allowed
    
    * remove unused time import
    
    * scookies -> _scookies
    
    * add check so overflowing number of futures is not silent
    
    * cleanup
    
    * remove unused operator import
    
    * use newest import
    
    * operator import is actually required
    
    * make new code flake8 compliant
    
    * update
    
    * add EP name registration
    
    * projections support for groups
    
    * added CkRegisterMainChare to list of methods that are now correctly traced
    
    * section entry methods are now traced
    
    * added registration to ArrayMaps
    
    * add registration support for ArrayMap
    
    * Add data required for projections to registration
    
    * Removed global entryNames/entryNamesLen. Unnecessary 'emStart'
      parameter removed
    
    * Moved entry method name tracking inside charmlib
    
    * Do we want to free the memory?
    
    * Add '--enable-tracing' option to setup.py
    
    * Checkout the charm4py_projections branch for running tests
    
    * Temporarily disable the ctypes interface in testing
    
    * Add tracing of user events
    
    * Add object store implementation
    
    * Buffer task until object is available
    
    * Fix buffering messages on target PE
    
    * Fix segmentation fault with object store
    
    * Add some documentation for the object store
    
    * Implemented waiting for futures
    
    * Add parameter server training example
    
    * Add requested flag to futures to avoid multiple requests for the same object
    
    * Add ray.wait function
    
    * Add async parameter server
    
    * Implement ray.put function to insert object in the object store
    
    * Merge ray futures and charm futures
    
    * Add store option to futures
    
    * Check future arguments in group methods
    
    * Try ray tasks implementation using charm4py pool
    
    * pool example
    
    * Remove checking for futures from groups
    
    * Stateless tasks and exit calls work
    
    * Working stateless tasks implementation
    
    * Add more examples
    
    * Fix ray.get calls from remote functions
    
    * Add fib example to debug MPI issue
    
    * For small objects, store them at home
    
    * Revert "merge ray into projections support"
    
    This reverts commit 44acea453c52ae8ada3e4937856c07716e178ccf, reversing
    changes made to 4f65ec6200792261a7bbda802e5a0c5284c3eb62.
    
    undo merge of ray due to compilation issues
    
    * remove generated files
    
    * merge fixes
    
    * merge fixes 2
    
    * merge fixes 3
    
    * add back entry names to libRegisterFunc:
    
    ---------
    
    Co-authored-by: Zane Fink <finkzane@gmail.com>
    Co-authored-by: Aditya Bhosale <adityapb1546@gmail.com>
    Co-authored-by: Ritvik Rao <rao1@dt-login01.delta.ncsa.illinois.edu>
    Co-authored-by: Ritvik Rao <rao1@dt-login02.delta.ncsa.illinois.edu>
    Co-authored-by: Ritvik Rao <ritvikrao@li-4c4c4544-0039-4610-8033-b2c04f574833.ibm.com>

--- charm4py/charmlib/charmlib_cython.pyx
+++ charm4py/charmlib/charmlib_cython.pyx
@@ -12,6 +12,11 @@ from cpython.ref cimport Py_INCREF
 from cpython.mem cimport PyMem_Malloc, PyMem_Realloc, PyMem_Free
 from cython.operator cimport dereference
 
+cdef extern from "Python.h":
+    char* PyUnicode_AsUTF8(object unicode)
+
+from libc.string cimport strcmp
+
 from ..charm import Charm4PyError
 from .. import reduction as red
 from cpython cimport array
@@ -107,6 +112,17 @@ ctypedef struct CkReductionTypesExt:
   int external_py
 
 cdef extern CkReductionTypesExt charm_reducers
+
+cdef char ** to_cstring_array(list_str):
+    cdef char **ret = <char **>malloc(len(list_str) * sizeof(char *))
+    for i in range(len(list_str)):
+        ret[i] = list_str[i]
+    return ret
+
+def addStringsToList(targetList, strings):
+  for idx, currentString in enumerate(strings):
+    targetList.append(<char*> malloc(len(currentString)))
+    targetList[idx] = strings[idx]
 
 class CkReductionTypesExt_Wrapper:
 
@@ -347,6 +363,8 @@ class CharmLib(object):
     self.direct_copy_supported = (PY_MAJOR_VERSION >= 3)
     self.name = 'cython'
     self.chareNames = []
+    self.emNames = []
+    self.emStart = 0
     self.init()
     self.ReducerType = CkReductionTypesExt_Wrapper()
     #print(charm_reducers.sum_long, charm_reducers.product_ushort, charm_reducers.max_char, charm_reducers.max_float, charm_reducers.min_char)
@@ -503,34 +521,51 @@ class CharmLib(object):
     if msg is None: CkRegisterReadonlyExt(n1, n2, 0, NULL)
     else: CkRegisterReadonlyExt(n1, n2, len(msg), msg)
 
-  def CkRegisterMainchare(self, str name, int numEntryMethods):
+  def CkRegisterMainchare(self, str name, list entryMethodNames, int numEntryMethods):
     self.chareNames.append(name.encode())
     cdef int chareIdx, startEpIdx
-    CkRegisterMainChareExt(self.chareNames[-1], numEntryMethods, &chareIdx, &startEpIdx)
+    self.emNames += [name.encode() for name in entryMethodNames]
+    # TODO: do we want to track/free these pointers?
+    cdef char** c1 = to_cstring_array(self.emNames)
+    CkRegisterMainChareExt(self.chareNames[-1], c1, self.emStart, numEntryMethods, &chareIdx, &startEpIdx)
+    self.emStart = len(self.emNames)
     return chareIdx, startEpIdx
 
-  def CkRegisterGroup(self, str name, int numEntryMethods):
+  def CkRegisterGroup(self, str name, list entryMethodNames, int numEntryMethods):
     self.chareNames.append(name.encode())
     cdef int chareIdx, startEpIdx
-    CkRegisterGroupExt(self.chareNames[-1], numEntryMethods, &chareIdx, &startEpIdx)
+    self.emNames += [name.encode() for name in entryMethodNames]
+    cdef char** c1 = to_cstring_array(self.emNames)
+    CkRegisterGroupExt(self.chareNames[-1], c1, self.emStart, numEntryMethods, &chareIdx, &startEpIdx)
+    self.emStart = len(self.emNames)
     return chareIdx, startEpIdx
 
-  def CkRegisterSectionManager(self, str name, int numEntryMethods):
+  def CkRegisterSectionManager(self, str name, list entryMethodNames, int numEntryMethods):
     self.chareNames.append(name.encode())
     cdef int chareIdx, startEpIdx
-    CkRegisterSectionManagerExt(self.chareNames[-1], numEntryMethods, &chareIdx, &startEpIdx)
+    self.emNames += [name.encode() for name in entryMethodNames]
+    cdef char** c1 = to_cstring_array(self.emNames)
+
+    CkRegisterSectionManagerExt(self.chareNames[-1], c1, self.emStart, numEntryMethods, &chareIdx, &startEpIdx)
+    self.emStart = len(self.emNames)
     return chareIdx, startEpIdx
 
-  def CkRegisterArrayMap(self, str name, int numEntryMethods):
+  def CkRegisterArrayMap(self, str name, list entryMethodNames, int numEntryMethods):
     self.chareNames.append(name.encode())
     cdef int chareIdx, startEpIdx
-    CkRegisterArrayMapExt(self.chareNames[-1], numEntryMethods, &chareIdx, &startEpIdx)
+    self.emNames += [name.encode() for name in entryMethodNames]
+    cdef char** c1 = to_cstring_array(self.emNames)
+    CkRegisterArrayMapExt(self.chareNames[-1], c1, self.emStart, numEntryMethods, &chareIdx, &startEpIdx)
+    self.emStart = len(self.emNames)
     return chareIdx, startEpIdx
 
-  def CkRegisterArray(self, str name, int numEntryMethods):
+  def CkRegisterArray(self, str name, list entryMethodNames, int numEntryMethods):
     self.chareNames.append(name.encode())
     cdef int chareIdx, startEpIdx
-    CkRegisterArrayExt(self.chareNames[-1], numEntryMethods, &chareIdx, &startEpIdx)
+    self.emNames += [name.encode() for name in entryMethodNames]
+    cdef char** c1 = to_cstring_array(self.emNames)
+    CkRegisterArrayExt(self.chareNames[-1], c1, self.emStart, numEntryMethods, &chareIdx, &startEpIdx)
+    self.emStart = len(self.emNames)
     return chareIdx, startEpIdx
 
   def CkCreateGroup(self, int chareIdx, int epIdx, msg not None):
@@ -851,6 +886,20 @@ class CharmLib(object):
 
   def scheduleTagAfter(self, int tag, double msecs):
     CcdCallFnAfter(CcdCallFnAfterCallback, <void*>tag, msecs)
+
+  def traceRegisterUserEvent(self, str EventDesc, int eventNum=-1):
+    cdef bytes py_bytes = EventDesc.encode()
+    Py_INCREF(py_bytes)
+    # This memory needs to be managed somehow, I think
+    cdef char* c_string = py_bytes
+    cdef int eventID = CkTraceRegisterUserEvent(c_string, eventNum)
+    return eventID
+
+  def traceBeginUserBracketEvent(self, int EventID):
+    CkTraceBeginUserBracketEvent(EventID)
+
+  def traceEndUserBracketEvent(self, int EventID):
+    CkTraceEndUserBracketEvent(EventID)
 
   def CcsRegisterHandler(self, str handlername, object handler):
     cdef bytes handler_bytes = handlername.encode("utf-8")
